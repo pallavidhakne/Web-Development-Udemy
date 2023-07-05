7b41814e188e021372f0cd8258dbf2b1807fda83
Pallavi Dhakne <85501739+pallavidhakne@users.noreply.github.com>
2023-07-05 15:49:02 +0530

    Create task1_lifeInWeekCoding.js

---
+++ JavaScript/task1_lifeInWeekCoding.js
@@ -0,0 +1,14 @@
+function lifeInWeeks(age) {
+    
+
+    
+    var year = 90-age;
+    var day=year*365;
+    var week=52*year;
+    var month=12*year;
+    
+    console.log("You have " + day + " days, " + week + " weeks, and " + month + " months left. ");
+    
+
+}
+
