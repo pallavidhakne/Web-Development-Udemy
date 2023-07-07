3128fba709d66386c36cada79646ca4f45cf78e7
Pallavi Dhakne <85501739+pallavidhakne@users.noreply.github.com>
2023-07-07 17:44:58 +0530

    Create higherOrderFunc.js

---
+++ JavaScript/higherOrderFunc.js
@@ -0,0 +1,20 @@
+function add(num1,num2)
+{
+  return num1+num2;
+}
+function sub(num1,num2)
+{
+  return num1-num2;
+}
+function multi(num1,num2)
+{
+  return num1*num2;
+}
+function divi(num1,num2)
+{
+  return num1/num2;
+}
+function calculator(num1,num2,operator)
+{
+   return operator(num1,num2);
+}
